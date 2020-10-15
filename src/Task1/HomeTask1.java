3745e71fc27cdfc898e134686f677f168ddaf31b
Inna <Zotova7inna@gmail.com>
2020-10-15 15:50:34 +0300

    init commit

---
+++ src/Task1/HomeTask1.java
@@ -0,0 +1,18 @@
+package Task1;
+
+public class HomeTask1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Inna");
+        System.out.println("Song - Strangers in the night");
+        System.out.println("Wondering in the night");
+        System.out.println("What were the chances");
+        System.out.println("We'd be sharing love");
+        System.out.println("Before the night was through");
+        System.out.println("Something in your eyes");
+        System.out.println("Was so inviting");
+        System.out.println("Something in your smile");
+        System.out.println("Was so exciting");
+        System.out.println("Applause");
+        System.out.println("Have a nice day!");
+    }
+}
---
+++ src/Task1/HomeTask2.java
@@ -0,0 +1,23 @@
+package Task1;
+
+public class HomeTask2 {
+        public static void main(String[] args) {
+            System.out.println("zzzzzzzzzzzzz" + "    " + "iiiii" + "       " + "iiiii");
+            System.out.println("zzzzzzzzzzzzz" + "    " + "iiiii" + "       " + "iiiii");
+            System.out.println("          zzz" + "    " + "iiiii" + "       " + "iiiii");
+            System.out.println("         zzz" + "     " + "     " + "       " + "     ");
+            System.out.println("        zzz" + "      " + "iiiii" + "       " + "iiiii");
+            System.out.println("       zzz" + "       " + "iiiii" + "       " + "     ");
+            System.out.println("      zzz" + "        " + "iiiii" + "       " + "iiiii");
+            System.out.println("     zzz" + "         " + "iiiii" + "       " + "iiiii");
+            System.out.println("    zzz" + "          " + "iiiii" + "       " + "iiiii");
+            System.out.println("   zzz" + "           " + "iiiii" + "       " + "iiiii");
+            System.out.println("  zzz" + "            " + "iiiii" + "       " + "iiiii");
+            System.out.println(" zzz" + "             " + "iiiii" + "       " + "iiiii");
+            System.out.println("zzz" + "              " + "iiiii" + "       " + "iiiii");
+            System.out.println("zzzzzzzzzzzzz" + "    " + "iiiii" + "       " + "iiiii");
+            System.out.println("zzzzzzzzzzzzz" + "    " + "iiiii" + "       " + "iiiii");
+        }
+    }
+
+
