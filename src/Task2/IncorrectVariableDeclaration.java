a479caa6473af7053e0e80bc9933943a76c70ae6
Inna <Zotova7inna@gmail.com>
2020-10-19 22:53:17 +0300

    add Task2

---
+++ src/Task2/IncorrectVariableDeclaration.java
@@ -0,0 +1,20 @@
+package Task2;
+/*
+ * Task:
+ * Uncomment code and fix compiler issues. Figure out the causes of failures.
+ * Hint: IDE usually provides a description of compiler issues.
+ */
+public class IncorrectVariableDeclaration {
+
+    public static void main(String[] args) {
+    int i = 0;
+    char c = 'c';
+    System.out.println("'c'");
+    int a = 3;
+    float f = 2.1F;
+    String s = "This is a string";
+    long bigLongValue = 964238764145245237L;
+    int bigInt = 20;
+    boolean isVariableAssignedInRightWay = true;
+    }
+}
